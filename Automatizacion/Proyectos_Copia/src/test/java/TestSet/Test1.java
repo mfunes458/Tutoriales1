3c6d8d6327d8a5c1c20c516a1372a67566ee416f
mfunes <PORTAL@gmail.com>
2019-05-16 16:57:54 -0300

    SUBI

--- Automatizacion/Proyectos_Copia/src/test/java/TestSet/Test1.java
+++ Automatizacion/Proyectos_Copia/src/test/java/TestSet/Test1.java
@@ -35,14 +35,14 @@ private WebDriver driver;
 	
 	//LINK
  			
-	  private By linkVerDetalles=By.xpath("/html[1]/body[1]/main[1]/div[2]/div[1]/div[1]/div[1]/a[1]"); 
+	  private By linkVerDetalles=By.xpath("/html[1]/body[1]/main[1]/div[2]/form[1]/button[1]/div[1]/div[1]/div[1]/span[1]"); 
 	  private By linkCLARO=By.partialLinkText("inicio"); 
 	  
 	 
 	
 	
 	//link 
-	private By linkVerMas=By.xpath("/html[1]/body[1]/main[1]/div[2]/div[1]/div[1]/div[1]/a[1]"); 
+	private By linkVerMas=By.xpath("/html[1]/body[1]/main[1]/div[2]/form[1]/button[1]/div[1]/div[1]/div[1]/span[1]"); 
 	private By prestamo10=By.xpath("/html[1]/body[1]/main[1]/div[2]/div[1]/div[1]/div[1]/div[1]/p[1]");
 	private By prestamo21=By.xpath("/html[1]/body[1]/main[1]/div[2]/div[1]/div[1]/div[2]/div[1]/p[1]");
 	private By linkClickAca=By.xpath("/html[1]/body[1]/main[1]/div[1]/div[1]/p[1]/a[1]");
